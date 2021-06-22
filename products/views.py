7ab94039272962e7b1b8cc890eeb3ed4e7a34d4e
Paul Istratoaie <p4ul.gg@gmail.com>
2021-06-22 11:10:34 +0100

    Add sorting and products count to product list

--- products/views.py
+++ products/views.py
@@ -20,11 +20,15 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
+            else:
+                direction = 'asc'
 
             products = products.order_by(sortkey)
 
