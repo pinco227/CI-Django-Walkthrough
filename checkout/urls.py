7aa3f8c6d232f0828ab109fdc519daac2e061c7b
Paul Istratoaie <p4ul.gg@gmail.com>
2021-06-30 22:14:50 +0100

    Make add product view, url and template

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,7 +4,8 @@ from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-    path('success/<order_number>', views.checkout_success, name='checkout_success'),
+    path('success/<order_number>/',
+         views.checkout_success, name='checkout_success'),
     path('cache_checkout_data/', views.cache_checkout_data,
          name='cache_checkout_data'),
     path('wh/', webhook, name='webhook'),
