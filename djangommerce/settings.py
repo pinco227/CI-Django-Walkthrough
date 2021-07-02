7c51343b97f59dd1b332a631bb3f28d979c711bc
Paul Istratoaie <p4ul.gg@gmail.com>
2021-07-02 09:45:58 +0100

    Setup postgres database

--- djangommerce/settings.py
+++ djangommerce/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import environ
 import os
+import dj_database_url
 
 env = environ.Env()
 environ.Env.read_env()
@@ -122,11 +123,15 @@ WSGI_APPLICATION = 'djangommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(env('PG_DB', default=''))
 }
 
 
